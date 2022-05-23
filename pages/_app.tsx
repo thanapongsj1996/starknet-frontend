45787ccd02e2c3076d13c29f9e2768e88c961770
Thanapong Somjai <thanapongsomjai@Thanapongs-MacBook-Pro.local>
2022-05-23 16:46:26 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import NextHead from 'next/head'
+import { InjectedConnector, StarknetProvider } from '@starknet-react/core'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const connectors = [new InjectedConnector()]
+
+  return (
+    <StarknetProvider autoConnect connectors={connectors}>
+      <Component {...pageProps} />
+    </StarknetProvider>
+  )
 }
 
 export default MyApp
