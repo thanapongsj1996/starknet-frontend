8f8e02912ff8c52331adbeea04b7a1bff47a8a2f
Thanapong Somjai <thanapongsomjai@Thanapongs-MacBook-Pro.local>
2022-05-24 00:20:44 +0700

    update frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,18 @@ const { compileCalldata } = stark
 // const userBalanceContractAddr = '0x062df77e8b24159df7a92981caf07c0c83c3574a1a329204884081dbdacd54da'
 const userBalanceContractAddr = '0x0428cfc80cb1eede2384003f4ca4ac516d00efec11a9865d52b0c8d3a005f270'
 
+type Starknet = IStarknetWindowObject | null
+
 const Home: NextPage = () => {
 
-  const [starknet, setStarknet] = useState(null as IStarknetWindowObject | null)
   const [amount, setAmount] = useState(0)
   const [curBalance, setCurBalance] = useState(0)
+  const [starknet, setStarknet] = useState(null as Starknet)
 
   const connectWallet = async () => {
     connect({ modalOptions: { theme: 'dark' } })
       .then((res) => {
-        setStarknet(res as IStarknetWindowObject)
+        setStarknet(res as Starknet)
         res?.enable()
       })
       .finally(() => console.log('address: ', starknet?.account?.address))
@@ -27,7 +29,7 @@ const Home: NextPage = () => {
   const getBalance = async () => {
     console.log('address: ', starknet?.account?.address)
 
-    if (!starknet || !starknet.account || !starknet.account.address) connectWallet()
+    isWalletConnected()
 
     const data = await starknet?.provider.callContract({
       contractAddress: userBalanceContractAddr,
@@ -47,13 +49,17 @@ const Home: NextPage = () => {
   const updateBalance = async (fnName: string) => {
     console.log('address: ', starknet?.account?.address)
 
-    if (!starknet || !starknet.account || !starknet.account.address) connectWallet()
+    isWalletConnected()
 
     starknet?.account.execute({
       contractAddress: userBalanceContractAddr,
       entrypoint: fnName,
       calldata: [`${amount}`]
     })
+  }
+
+  const isWalletConnected = () => {
+    if (!starknet || !starknet.account || !starknet.account.address) connectWallet()
   }
 
   const getNumber = (num: string) => {
@@ -63,22 +69,30 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+
         <h1 className={styles.title}>
-          Wallet
+          Demo
         </h1>
-        {!starknet && <button onClick={connectWallet}>login</button>}
+
+        {!starknet &&
+          <button className={styles.btn} onClick={() => connectWallet()}>
+            Connect Wallet
+          </button>
+        }
+
         {starknet &&
           <>
-            <button onClick={getBalance}>Get Balance</button>
-            <span>Current Balance: {curBalance}</span>
+            <button className={styles.btn} onClick={() => getBalance()}>Get Balance</button>
+            <h2>Current Balance: {curBalance}</h2>
 
-            <input onChange={e => setAmount(parseInt(e.target.value))} />
+            <input className={styles.amount__input} onChange={e => setAmount(parseInt(e.target.value))} />
+            
             <div>
-              <button onClick={() => updateBalance('increase_balance')}>
-                Increase balance
+              <button className={styles.btn} onClick={() => updateBalance('increase_balance')}>
+                Increase Balance
               </button>
-              <button onClick={() => updateBalance('decrease_balance')}>
-                Decrease balance
+              <button className={styles.btn} onClick={() => updateBalance('decrease_balance')}>
+                Decrease Balance
               </button>
             </div>
           </>
